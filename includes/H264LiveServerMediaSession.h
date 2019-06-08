9a86a145a8082267fb5299d4c8f5b4772507da58
MathewRo <rohitpmathew@yahoo.co.in>
2019-06-08 21:09:19 +0530

    changed header structure

--- includes/H264LiveServerMediaSession.h
+++ includes/H264LiveServerMediaSession.h
@@ -7,22 +7,22 @@
 class OnDemandServerMediaSubSession;
 class H264LiveServerMediaSession:public OnDemandServerMediaSubsession
 {
-public:
-    static H264LiveServerMediaSession* createNew(UsageEnvironment& env, bool reuseFirstSource);
-    void checkForAuxSDPLine1();
-    void afterPlayingDummy1();
-protected:
-    H264LiveServerMediaSession(UsageEnvironment& env, bool reuseFirstSource);
-    virtual ~H264LiveServerMediaSession(void);
-    void setDoneFlag() { fDoneFlag = ~0; }
-protected:
-    virtual char const* getAuxSDPLine(RTPSink* rtpSink, FramedSource* inputSource);
-    virtual FramedSource* createNewStreamSource(unsigned clientSessionId, unsigned& estBitrate);
-    virtual RTPSink* createNewRTPSink(Groupsock* rtpGroupsock, unsigned char rtpPayloadTypeIfDynamic, FramedSource* inputSource);
-private:
-    char* fAuxSDPLine;
-    char fDoneFlag;
-    RTPSink* fDummySink;
+	public:
+		static H264LiveServerMediaSession* createNew(UsageEnvironment& env, bool reuseFirstSource);
+		void checkForAuxSDPLine1();
+		void afterPlayingDummy1();
+	protected:
+		H264LiveServerMediaSession(UsageEnvironment& env, bool reuseFirstSource);
+		virtual ~H264LiveServerMediaSession(void);
+		void setDoneFlag() { fDoneFlag = ~0; }
+	protected:
+		virtual char const* getAuxSDPLine(RTPSink* rtpSink, FramedSource* inputSource);
+		virtual FramedSource* createNewStreamSource(unsigned clientSessionId, unsigned& estBitrate);
+		virtual RTPSink* createNewRTPSink(Groupsock* rtpGroupsock, unsigned char rtpPayloadTypeIfDynamic, FramedSource* inputSource);
+	private:
+		char* fAuxSDPLine;
+		char fDoneFlag;
+		RTPSink* fDummySink;
 };  
 
 #endif
--- includes/LiveSourceWithx264.h
+++ includes/LiveSourceWithx264.h
@@ -7,20 +7,20 @@
 
 class LiveSourceWithx264:public FramedSource
 {
-public:
-    static LiveSourceWithx264* createNew(UsageEnvironment& env);
-    static EventTriggerId eventTriggerId;
-protected:
-    LiveSourceWithx264(UsageEnvironment& env);
-    virtual ~LiveSourceWithx264(void);
-private:
-    virtual void doGetNextFrame();
-    static void deliverFrame0(void* clientData);
-    void deliverFrame();
-    void encodeNewFrame();
-    static unsigned referenceCount;
-    std::queue<x264_nal_t *> nalQueue;
-    timeval currentTime;
-    x264Encoder *encoder;
+	public:
+		static LiveSourceWithx264* createNew(UsageEnvironment& env);
+		static EventTriggerId eventTriggerId;
+	protected:
+		LiveSourceWithx264(UsageEnvironment& env);
+		virtual ~LiveSourceWithx264(void);
+	private:
+		virtual void doGetNextFrame();
+		static void deliverFrame0(void* clientData);
+		void deliverFrame();
+		void encodeNewFrame();
+		static unsigned referenceCount;
+		std::queue<x264_nal_t *> nalQueue;
+		timeval currentTime;
+		x264Encoder *encoder;
 }; 
 #endif
