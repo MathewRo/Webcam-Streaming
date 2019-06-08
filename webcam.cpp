5c75f4870c2229a8ea7f4c25d2017ea04bd93fe3
MathewRo <rohitpmathew@yahoo.co.in>
2019-06-08 21:10:38 +0530

    changed webam header path

--- webcam.cpp
+++ webcam.cpp
@@ -9,23 +9,23 @@
 #include "includes/LiveSourceWithx264.h"
 int main()
 {  
-    TaskScheduler* taskSchedular = BasicTaskScheduler::createNew();
-    BasicUsageEnvironment* usageEnvironment = BasicUsageEnvironment::createNew(*taskSchedular);
-    RTSPServer* rtspServer = RTSPServer::createNew(*usageEnvironment, 8554, NULL);
-    if(rtspServer == NULL)
-    {
-        *usageEnvironment << "Failed to create rtsp server ::" << usageEnvironment->getResultMsg() <<"\n";
-        exit(1);
-    }
-    std::string streamName = "webcam";
-    ServerMediaSession* sms = ServerMediaSession::createNew(*usageEnvironment, streamName.c_str(), streamName.c_str(), "Live H264 Stream");
-    H264LiveServerMediaSession *liveSubSession; 
-    liveSubSession = H264LiveServerMediaSession::createNew(*usageEnvironment, true);
-    sms->addSubsession(liveSubSession);
-    rtspServer->addServerMediaSession(sms);
-    char* url = rtspServer->rtspURL(sms);
-    *usageEnvironment << "Play the stream using url "<<url << "\n";
-    delete[] url;
-    taskSchedular->doEventLoop();
-    return 0;
+	TaskScheduler* taskSchedular = BasicTaskScheduler::createNew();
+	BasicUsageEnvironment* usageEnvironment = BasicUsageEnvironment::createNew(*taskSchedular);
+	RTSPServer* rtspServer = RTSPServer::createNew(*usageEnvironment, 8554, NULL);
+	if(rtspServer == NULL)
+	{
+		*usageEnvironment << "Failed to create rtsp server ::" << usageEnvironment->getResultMsg() <<"\n";
+		exit(1);
+	}
+	std::string streamName = "webcam";
+	ServerMediaSession* sms = ServerMediaSession::createNew(*usageEnvironment, streamName.c_str(), streamName.c_str(), "Live H264 Stream");
+	H264LiveServerMediaSession *liveSubSession; 
+	liveSubSession = H264LiveServerMediaSession::createNew(*usageEnvironment, true);
+	sms->addSubsession(liveSubSession);
+	rtspServer->addServerMediaSession(sms);
+	char* url = rtspServer->rtspURL(sms);
+	*usageEnvironment << "Play the stream using url "<<url << "\n";
+	delete[] url;
+	taskSchedular->doEventLoop();
+	return 0;
 }
