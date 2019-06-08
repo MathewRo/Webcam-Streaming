c18a6a83e738a3181d889d6dc188ef290e3e877e
MathewRo <rohitpmathew@yahoo.co.in>
2019-06-08 21:11:48 +0530

    added live555 and x264 library

---
+++ libraries/live/BasicUsageEnvironment/include/BasicUsageEnvironment_version.hh
@@ -0,0 +1,10 @@
+// Version information for the "BasicUsageEnvironment" library
+// Copyright (c) 1996-2019 Live Networks, Inc.  All rights reserved.
+
+#ifndef _BASICUSAGEENVIRONMENT_VERSION_HH
+#define _BASICUSAGEENVIRONMENT_VERSION_HH
+
+#define BASICUSAGEENVIRONMENT_LIBRARY_VERSION_STRING	"2019.05.29"
+#define BASICUSAGEENVIRONMENT_LIBRARY_VERSION_INT		1559088000
+
+#endif
